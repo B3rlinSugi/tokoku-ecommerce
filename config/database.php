989ab535dd44d39b7b4b74b1832bd6c8faeb95f9
Berlin Sugiyanto <berlinsugiyanto23@gmail.com>
2026-04-08 23:29:39 +0700

    fix: relax session cookie params for Railway proxy

--- config/database.php
+++ config/database.php
@@ -46,9 +46,7 @@ if (session_status() === PHP_SESSION_NONE) {
     session_set_cookie_params([
         'lifetime' => 0,
         'path'     => '/',
-        'secure'   => true,
         'httponly' => true,
-        'samesite' => 'Strict',
     ]);
     session_start();
 }
